f64cb4d89b09b426cf2314096b02240b9638d4c3
sadxzyq <satriopambudi866@gmail.com>
2023-03-21 07:40:38 +0700

    darxv6

---
+++ p.sh
@@ -0,0 +1,6 @@
+git config --global --add safe.directory /storage/emulated/0/zbot
+git branch -M master
+git add *
+git commit -m "darxv6"
+git remote add origin https://github.com/sadxzyq/darxv6.git
+git push --all
